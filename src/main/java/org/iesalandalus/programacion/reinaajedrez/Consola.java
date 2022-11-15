9d4a11b644e4cba77777e432a3e60ea92a534adf
RubenNicolas85 <117572476+RubenNicolas85@users.noreply.github.com>
2022-11-15 23:28:41 +0100

    Update Consola.java
    CLASE CONSOLA: 2. Crea el constructor para esta clase con su visibilidad adecuada, teniendo en cuenta que será una clase de utilidades que sólo contendrá métodos estáticos. Realiza un commit.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,5 +1,12 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 public class Consola {
-
+	
+	/* Creamos constructor de la clase sin parámetros y sin almacenamiento en variables, ya que la clase es sólo para 
+	 * métodos y no tiene atributos */
+	
+	private Consola() {
+		
+		
+	}
 }
