dcaeb324d04c9acdc00c36c1492bbf3a0962fb6c
RubenNicolas85 <117572476+RubenNicolas85@users.noreply.github.com>
2022-11-15 11:34:10 +0100

    Create Posicion.java
    CLASE POSICION: 1. Crea la clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada. Realiza un commit.

---
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Posicion {
+
+	// Creamos las variables de clase o atributos y los encapsulamos con el modificador de acceso private
+	
+	private int fila; 
+	private char columna; 
+	
+}
