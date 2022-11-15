a117cf3bb97e1cccbd9652cab8b2f80c2f2a882b
RubenNicolas85 <117572476+RubenNicolas85@users.noreply.github.com>
2022-11-15 23:39:49 +0100

    Update Consola.java
    CLASE CONSOLA: 3. Crea el método mostrarMenu que mostrará el menú con las opciones de nuestra aplicación: crear reina por defecto, crear reina eligiendo el color, mover y salir. Realiza un commit.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -9,4 +9,13 @@ public class Consola {
 		
 		
 	}
+	
+	/* Se crea el método estático mostrarMenu que mostrará el menú con las opciones de nuestra aplicación: crear reina 
+	 * por defecto, crear reina eligiendo el color, mover y salir */
+	
+	public static String mostrarMenu() {
+		 
+		return "MENÚ PRINCIPAL DE LA APLICACIÓN REINA DE AJEDREZ: \n1. Crear reina por defecto"
+				+ "\n2. Crear reina eligiendo el color \n3. Mover \n4. Salir";
+	}
 }
