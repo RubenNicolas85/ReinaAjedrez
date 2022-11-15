1916e6b734a6c5ca6e3f11fedb19007c5df1fdf6
RubenNicolas85 <117572476+RubenNicolas85@users.noreply.github.com>
2022-11-15 13:03:08 +0100

    Create Reina.java
    CLASE REINA: 1. Crea la clase Reina cuyos atributos serán un color (del tipo enumerado Color) y posicion (de la clase Posicion), con la visibilidad adecuada. Realiza un commit.

---
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Reina {
+	
+	/* Establecemos las variables o atributos de la clase Reina, serán color (del enum Color) y posición
+	 *  (de la clase Posición) */
+	
+	private Color color; 
+	private Posicion posicion; 
+	
+}
